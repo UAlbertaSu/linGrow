5618116225e4adf4da9242674af9b6d3d412a190
Sam <rittwage@ualberta.ca>
2022-10-08 16:06:46 -0600

    Early_iterating

---
+++ lingrow_frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="Lingrow_Login">
+      <header className="App-header">
+        <p>
+          Lingrow
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
