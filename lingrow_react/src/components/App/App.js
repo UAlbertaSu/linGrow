598d552141972c12586ab821276d5fbc99a50111
Scott Han <shhan@ualberta.ca>
2022-10-10 10:46:06 -0600

    Implemented session storage

--- lingrow_react/src/components/App/App.js
+++ lingrow_react/src/components/App/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import './App.css';
 import Login from '../Login/Login';
 import Dashboard from '../Dashboard/Dashboard';
 import Preferences from '../Preferences/Preferences';
-
-function setToken(userToken) {
-  sessionStorage.setItem('token', JSON.stringify(userToken));
-}
-
-function getToken() {
-  const tokenString = sessionStorage.getItem('token');
-  const userToken = JSON.parse(tokenString);
-  return userToken?.token
-}
+import useToken from './useToken.js';
 
 function App() {
-  const token = getToken();
+  const { token, setToken } = useToken();
 
   if (!token) {
     return <Login setToken={setToken} />
