ea2230ed76832eacb8be840cfb7d76451df8ef0f
Sam <rittwage@ualberta.ca>
2022-10-11 19:15:02 -0600

    Signup Page Changed

--- lingrow_react/src/components/Signup/Signup.js
+++ lingrow_react/src/components/Signup/Signup.js
@@ -1,7 +1,10 @@
 import { useState } from 'react';
 import './Signup.css';
+import 'react-bootstrap/Form'
+import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
 
-export default function Form() {
+  export default function Form() {
 
     // States for registration
     const [name, setName] = useState('');
@@ -48,34 +51,46 @@ export default function Form() {
     // Showing error message if error is true
     const errorMessage = () => {
         return (
-            <div className="error" style={{
-                display: error ? '' : 'none',
-            }}>
-                <h1>Please enter all the fields</h1>
-            </div>
+            <div class="container">
+                <div class="center"></div>
+                    <div className="error" style={{
+                    display: error ? '' : 'none',
+                    }}>
+                    <h1>User Registration</h1>
+                    Please enter all the fields
+                    </div>
+                </div>
         );
     };
 
     return (
-        <div className="form">
-            <div className="messages">
-                <h1>User Registration</h1>
-            </div>
+        <Container>
+            <div class="container">
+                <div class="center">
+                    <div className="form">
+                        <div className="messages">
+                            <h1>User Registration</h1>
+                        </div>
 
-            {/* Calling to the methods */}
-            <div className="messages">
-                {errorMessage()}
-                {successMessage()}
+                        {/* Calling to the methods */}
+                        <div className="messages">
+                            {errorMessage()}
+                            {successMessage()}
+                        </div>
+                        <form classname='input'>
+                            <div class="form-group">
+                                {/* Labels and inputs for form data */}
+                                <label className="label">Name</label>
+                                <input type="text" class="form-control" id="name" placeholder="Enter name" value={name} onChange={handleName} />
+                                <label className="label">Password</label>
+                                <input type="password" class="form-control" id="password" placeholder="Enter password" value={password} onChange={handlePassword} />
+                            </div>
+                            <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
+                        </form>
+                    </div>
+                </div>
             </div>
-
-            <form className="input">
-                {/* Labels and inputs for form data */}
-                <label className="label">Name</label>
-                <input onChange={handleName} className="input" value={name} type="text" />
-                <label className="label">Password</label>
-                <input onChange={handlePassword} className="input" value={password} type="password" />
-                <button onClick={handleSubmit} type="submit">Submit</button>
-            </form>
-        </div>
+      </Container>
+            
     );
 }
