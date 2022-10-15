3d6fa01d4d89651ec0c170d0f26bcf310f3cf914
Scott Han <shhan@ualberta.ca>
2022-10-15 03:51:10 -0600

    Login & Signup working locally

--- lingrow_react/src/components/Login/Login.js
+++ lingrow_react/src/components/Login/Login.js
@@ -1,28 +1,30 @@
 import React, { useState } from 'react';
-import { useNavigate } from "react-router-dom";
-import PropTypes from 'prop-types';
+import { Navigate, useNavigate } from "react-router-dom";
+// import PropTypes from 'prop-types';
 import { Form, Button, Card } from 'react-bootstrap';
 
 import logo from "../Img/lingrow.png";
 import './Login.css';
-import useToken from '../App/useToken.js';
+// import useToken from '../App/useToken.js';
 
 async function loginUser(credentials) {
+    console.log(JSON.stringify(credentials));
     return fetch('http://127.0.0.1:8000/api/user/login/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(credentials)
-    })
-        .then(data => data.json())
+    }).then(data => data.json())
 }
 
 export default function Login() {
     const nav = useNavigate();
-    const{setToken} = useToken();
+    // const[token, setToken] = useToken();
     const[email, setEmail] = useState();
     const[password, setPassWord] = useState();
+    const[error, setError] = useState(false);
+
 
     const handleSubmit = async (event) =>{
         event.preventDefault();
@@ -30,17 +32,33 @@ export default function Login() {
             "email": email,
             "password": password
         });
+        sessionStorage.setItem('token', JSON.stringify(token));
 
-            
-        setToken(token);
-        console.log(token);
-        nav("/dashboard");
+        if (sessionStorage.getItem('token').includes("error")) {
+            setError(true);
+            console.log("Validation failed");
+        }
+        else {
+            setError(false);
+            console.log("Valid credentials");
+            nav("/dashboard");
+        }
     }
 
     const redirectToSignup = async (event) => {
         event.preventDefault();
         nav("/signup");
     }
+
+    const errorMessage = () => {
+        return (
+            <div className="error" style={{
+            display: error ? '' : 'none',
+            }}>
+                Invalid email or password
+            </div>
+        );
+    };
 
     return (
         <Card>
@@ -50,6 +68,9 @@ export default function Login() {
             <input type="text" class="form-control" id="name" placeholder="Enter name" onChange={e => setEmail(e.target.value)}/>
             <label className="label">Password</label>
             <input type="password" class="form-control" id="password" placeholder="Enter Password" onChange={e => setPassWord(e.target.value)}/>
+            <div className="message">
+                {errorMessage()}
+            </div>
             <Button variant="primary" type="submit" onClick={handleSubmit}>Login</Button>
             <Button variant="primary" type="submit" onClick={redirectToSignup}>Sign Up</Button>
             <a href="https://drive.google.com/drive/folders/1h4pmfp66la3ZBpEIwcfHb7TEY5QbUgOj">Kitchen Activities</a>
@@ -58,6 +79,6 @@ export default function Login() {
     )
 }
 
-Login.propTypes = {
-    setToken: PropTypes.func.isRequired
-};
+// Login.propTypes = {
+//     setToken: PropTypes.func.isRequired
+// };
