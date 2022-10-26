e8c5e6457c93fb1e346f3577b35d3a3b67845bb3
Loyaljot Singh Bajwa <55115192+bajwa10@users.noreply.github.com>
2022-10-26 17:16:13 -0400

    Update urls.py

--- lingrow/lingrow/urls.py
+++ lingrow/lingrow/urls.py
@@ -18,7 +18,6 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('admin_school_management.urls')),
     path('api/user/', include('account.urls')),
     path('api/school/', include('admin_school_management.urls')),
     path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
