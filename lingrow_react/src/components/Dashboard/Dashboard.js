37f3993b467e36f7081d0e5ff01d38c35a7fac92
Scott Han <shhan@ualberta.ca>
2022-11-13 15:05:29 -0700

    Update Dashboard.js

--- lingrow_react/src/components/Dashboard/Dashboard.js
+++ lingrow_react/src/components/Dashboard/Dashboard.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 import './Dashboard.css';
 
 import DashboardAdmin from './DashboardAdmin';
 import DashboardParent from './DashboardParent';
 import DashboardTeacher from './DashboardTeacher';
 import DashboardResearcher from './DashboardResearcher';
+import Login from '../Login/Login';
 
 export default function Dashboard() {
+    const nav = useNavigate();
     let userType = parseInt(sessionStorage.getItem('userType'));
 
-    // navigate based on appropriate user type
+    // navigate based on appropriate user type, or login page if user hasn't logged in yet.
     switch(userType) {
         case 1:
             return (<div><DashboardParent /></div>);
@@ -19,5 +22,7 @@ export default function Dashboard() {
             return (<div><DashboardResearcher /></div>);
         case 4:
             return (<div><DashboardAdmin /></div>);
+        default:
+            return (<div><Login /></div>);
     }
 }
