e9e7067c30983b791ee8f0fa816ea3ec3a3cc4d7
Kevin Zhu <kjzhu@ualberta.ca>
2022-11-19 18:29:42 -0700

    added unfinished user info page for parent

--- lingrow_react/src/components/UserInfo/UserInfo.js
+++ lingrow_react/src/components/UserInfo/UserInfo.js
@@ -7,6 +7,8 @@ import Translate from '../Translate/Translate';
 // Group manager component.
 export default function UserInfo(AccountType) {
 
+    // TODO: need to link to profile button
+
     // TODO: implement hiding of elements based on account type
     AccountType = 'Parent';
 
@@ -19,10 +21,9 @@ export default function UserInfo(AccountType) {
     const [teachers_header, setTeachersHeader] = useState("Teacher(s)");
     const [classrooms_header, setClassroomsHeader] = useState("Classroom(s)");
     const [schools_header, setSchoolsHeader] = useState("School(s)");
-    const [delete_account_btn, setDeleteAccountBtn] = useState("Delete Account");
 
 
-    // TODO: Delete later, only for testing
+    // TODO: need to get data from backend
     const DUMMY_DATA = [
         {
             "user": {
@@ -34,37 +35,16 @@ export default function UserInfo(AccountType) {
                 "user_type": 1
             }
         },
-        {
-            "user": {
-                "id": 8,
-                "email": "parent2@email.com",
-                "first_name": "parent",
-                "middle_name": "",
-                "last_name": "parent",
-                "user_type": 1
-            }
-        },
-        {
-            "user": {
-                "id": 9,
-                "email": "parent3@email.com",
-                "first_name": "parent3",
-                "middle_name": "",
-                "last_name": "thirdParent",
-                "user_type": 1
-            }
-        }
     ]
 
 
-
     // textfields
-    // TODO: we need the specific parent id for this
-    // TODO: we need password (and childs name)
+    // TODO: we need to implement password changing
+    // TODO: we need to get child's name
     const [username_email_input, setUsernameEmailInput] = useState(DUMMY_DATA[0].user.email);
-    const [password_input, setPasswordInput] = useState("");
+    const [password_input, setPasswordInput] = useState("[Password]");
     const [name_input, setNameInput] = useState(DUMMY_DATA[0].user.first_name + " " + DUMMY_DATA[0].user.last_name);
-    const [childs_name_input, setChildsNameInput] = useState("");
+    const [childs_name_input, setChildsNameInput] = useState("[Child's Name]");
     
     
     // Setter for initial page translation.
@@ -81,7 +61,6 @@ export default function UserInfo(AccountType) {
             Translate('en', lang, "Teacher(s)").then(response => setTeachersHeader(response));
             Translate('en', lang, "Classroom(s)").then(response => setClassroomsHeader(response));
             Translate('en', lang, "School(s)").then(response => setSchoolsHeader(response));
-            Translate('en', lang, "Delete Account").then(response => setDeleteAccountBtn(response));    
         }
     });
 
@@ -98,14 +77,22 @@ export default function UserInfo(AccountType) {
     
 
     return (
-        <Card style={{minHeight:"fit-content"}}>
+        // TODO: add alignment 
+        <Card style={{minHeight:"fit-content", position: "relative"}}>
             <LanguageList />
-
+            <h5>{account_type_header}</h5>
+            <h5>{AccountType}</h5>
+            <h5>{username_email_header}</h5>
             <input type="text" className="form-control" id="username_email_input" placeholder={username_email_input} onChange={e => setUsernameEmailInput(e.target.value)}/>
+            <h5>{password_header}</h5>
             <input type="text" className="form-control" id="password_input" placeholder={password_input} onChange={e => setPasswordInput(e.target.value)} />
+            <h5>{name_header}</h5>
             <input type="text" className="form-control" id="name_input" placeholder={name_input} onChange={e => setNameInput(e.target.value)} />
+            <h5>{childs_name_header}</h5>
             <input type="text" className="form-control" id="childs_name_input" placeholder={childs_name_input} onChange={e => setChildsNameInput(e.target.value)} />
-     
+            <h5>{teachers_header}</h5>
+            <h5>{classrooms_header}</h5>
+            <h5>{schools_header}</h5>
         </Card>
     );
 }
