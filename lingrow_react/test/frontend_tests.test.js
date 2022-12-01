9238552f09e1e6303a267d0abd9adbfef6ca861d
JeffDongYang <ydong4@ualberta.ca>
2022-12-01 00:06:40 -0700

    1

--- lingrow_react/test/frontend_tests.test.js
+++ lingrow_react/test/frontend_tests.test.js
@@ -1076,3 +1076,51 @@ describe ('Searching users functionalities', async () => {
         }
     });
 });
+
+describe ('Searching users functionalities', async () => {
+    it ('Search user function should navigate to the corresponding page', async () => {
+        let browser = await puppeteer.launch({
+            headless: false
+        });
+        try {
+            let page = await browser.newPage(); 
+            await page.goto('http://localhost:3000/login');
+            page.evaluate(() => {
+                sessionStorage.setItem('token', 'frontend_test_token');
+            })  
+            await page.goto('http://localhost:3000/login', {waitUntil: 'networkidle0',});
+
+            const textbox = await page.$('#email'); //email input
+            await textbox.type('frontend@test.com');
+            const textbox2 = await page.$('#password'); //pswd input
+            await textbox2.type('Aa1234567!');
+            await new Promise(r => setTimeout(r, 500));
+            await page.waitForSelector('#login'); //login
+            await page.click('#login');
+            await new Promise(r => setTimeout(r, 500));
+
+            await page.waitForSelector('#searchUsers'); //Search users button
+            await page.click('#searchUsers');
+            await new Promise(r => setTimeout(r, 500));
+
+            const url1 = await page.evaluate(() => document.location.href); //Check if the page is redirected to the correct page
+            expect(url1).is.equal('http://localhost:3000/searchuser');
+
+            const textbox3 = await page.$('#searchUsername'); //Username input
+            await textbox3.type('frontendtest');
+
+            await page.waitForSelector('#searchstart'); //start searching users button
+            await page.click('#searchstart');
+            await new Promise(r => setTimeout(r, 800));
+
+            const url2 = await page.evaluate(() => document.location.href); //Check if the page is redirected to the correct page
+            expect(url2).is.equal('http://localhost:3000/searchuser');
+        }
+        catch (error) {
+            throw(error);
+        }
+        finally {
+            await browser.close();
+        }
+    });
+});
